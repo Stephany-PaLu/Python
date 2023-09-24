e07f10050cfd89005d52f6dcaba1c59f653e115d
Stephany-PaLu <stefy-07lisbeth@hotmail.es>
2023-09-24 00:08:46 -0500

    cargué los paquetes matplotlib como plt y pandas como pd

--- Tarea_1.py
+++ Tarea_1.py
@@ -9,9 +9,12 @@ use_condaenv("AmbienteTarea.yml")
 
 # a. Cargue un paquete de su elección - Recuerden que debe estar disponible en su ambiente
 import numpy as np
-
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # b. Dos variables, una que contenga su nombre y la otra su edad
+
+
 # c. Crear una oración que indique su nombre y su edad utilizando las dos variables creadas en el paso anterior
 
 
