c0f9e39f397f671e556b249ee705557330827b7b
Stephany-PaLu <stefy-07lisbeth@hotmail.es>
2023-10-07 17:15:21 -0500

    guardé los datos de felicidad

--- Tarea_2.py
+++ Tarea_2.py
@@ -1,3 +1,7 @@
 import pandas as pd
 import numpy as np
 import glob as glob
+
+#obtener los datos sobre felicidad
+
+datos_felicidad = pd.read_csv('https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-08-07/felicidad.csv')
