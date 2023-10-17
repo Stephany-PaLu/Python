667f7708d7c825b9a334e46a37e3bb3b6d4e1a17
Stephany-PaLu <stefy-07lisbeth@hotmail.es>
2023-10-17 00:18:16 -0500

    agregué la base de datos 'partidos'

--- Tarea_3/Tarea_3.py
+++ Tarea_3/Tarea_3.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Acceder los datos de manera remota, es decir no bajándolos a su computadora.
 
-
-
-np.loadtxt('prueba.csv', delimiter = ',')
+datos_partidos= pd.read_csv('https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-04-10/partidos.txt',delimiter='\t')
+datos_partidos
